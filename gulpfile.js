d7bfe459b241350155adacc85373cf1156c7a43c
Oleksandr <Oleksbielianinov@gmail.com>
2016-07-09 16:05:03 +0300

    start done

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./markup/scss/all.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./production/css'));
+});
+ 
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', () =>
+	gulp.src('markup/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('production/images'))
+);
