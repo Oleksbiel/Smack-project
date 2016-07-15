45606ec621e7768db23e6861982154ec9e1d0df0
Sasha <Oleksbielianinov@gmail.com>
2016-07-15 10:27:27 +0300

    add Eugen's anchor links

--- markup/js/assets.js
+++ markup/js/assets.js
@@ -84,3 +84,24 @@ demo4.start();
 $(function() {
     $('.mg-space-init').mgSpace();
 });
+
+////////// Anchers
+
+$('.anchor-top').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({scrollTop: 0}, 1500);
+});
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
